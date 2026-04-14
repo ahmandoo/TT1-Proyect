ca6704d986b7f4dcc85485dc2b619e0d60676282
Javier24101 <javierrincon24101@gmail.com>
2026-04-14 18:32:37 +0200

    InterfazEnviarEmails documentado

--- src/main/java/interfaces/InterfazEnviarEmails.java
+++ src/main/java/interfaces/InterfazEnviarEmails.java
@@ -1,7 +1,15 @@
 package interfaces;
 
 import modelo.Destinatario;
-
+/**
+ * Interfaz que define el contrato para el servicio de envío de notificaciones por correo.
+ */
 public interface InterfazEnviarEmails {
+	/**
+     * Envía un mensaje de correo a un destinatario específico.
+     * * @param dest  Objeto que representa al destinatario del correo.
+     * @param email Contenido en texto del mensaje a enviar.
+     * @return true si el correo se envió con éxito, false en caso de fallo.
+     */
 	public boolean enviarEmail(Destinatario dest, String email);
 }
