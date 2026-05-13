2943967dcc36b3fbcf4fc8361532bfb1907a4f05
mapascleo <mapascleo@unirioja.es>
2026-05-13 12:36:41 +0200

    @Controller e import

--- src/main/java/com/tt1/trabajo/controller/RegistroController.java
+++ src/main/java/com/tt1/trabajo/controller/RegistroController.java
@@ -3,6 +3,7 @@ package com.tt1.trabajo.controller;
 import com.tt1.trabajo.entity.UsuarioEntity;
 import com.tt1.trabajo.repository.UsuarioRepository;
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
